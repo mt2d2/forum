c5b88ecff0823621609112005689e823cf59b224
Michael Tremel <itrekkie@gmail.com>
2015-03-04 00:37:59 -0700

    add new test file

---
+++ model/topic_test.go
@@ -0,0 +1,51 @@
+package model
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestEmptyTopic(t *testing.T) {
+	topic := NewTopic()
+	if !reflect.DeepEqual(topic, &Topic{-1, "", "", -1, -1, nil}) {
+		t.Error("topic not empty")
+	}
+}
+
+func TestFindOneTopic(t *testing.T) {
+	db, err := GetMockupDB()
+	defer db.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	topic, err := FindOneTopic(db, "1")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if topic.Id != 1 {
+		t.Error("wrong id")
+	}
+
+	if topic.Forum == nil {
+		t.Error("no forum relation")
+	}
+
+	if topic.ForumId != 1 ||
+		(topic.Forum != nil && topic.ForumId != topic.Forum.Id) {
+		t.Error("wrong forum")
+	}
+
+	if topic.Title != "test topic" {
+		t.Error("wrong title")
+	}
+
+	if topic.Description != "asdf asdf asdf" {
+		t.Error("wrong description")
+	}
+
+	if topic.PostCount != 11 {
+		t.Error("wrong post count")
+	}
+}
