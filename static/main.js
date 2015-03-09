a951e4f145f13e227a087212160893c45ed34ad8
Michael Tremel <itrekkie@gmail.com>
2015-03-08 18:23:06 -0700

    animate removal of status alert

--- static/main.js
+++ static/main.js
@@ -1,4 +1,10 @@
 $(function() {
+  function fadeOut(element) {
+    element.animate({ margin: 'hide', padding: 'hide', height: 'hide' }, 'fast', 'swing', function() {
+        $(this).remove()
+    });
+  }
+
   // inspired by http://stackoverflow.com/questions/22636819/confirm-delete-using-bootstrap-3-modal-box
   $('button[name="removePost"]').on('click', function(e) {
     var $form = $(this).closest('form');
@@ -7,5 +13,13 @@ $(function() {
       .one('click', '#delete', function() {
         $form.trigger('submit');
       });
+  });
+
+  var alertSuccess = $(".alert-success");
+  window.setTimeout(function() {
+    fadeOut(alertSuccess)
+  }, 4000);
+  $('button[class="close"]').on('click', function(e) {
+    fadeOut(alertSuccess);
   });
 });
