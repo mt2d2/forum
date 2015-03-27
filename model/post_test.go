b6689ca0adb51c2d4f8be883235040be9ce05318
Michael Tremel <itrekkie@gmail.com>
2015-03-26 19:57:02 -0700

    more tests

--- model/post_test.go
+++ model/post_test.go
@@ -1,6 +1,7 @@
 package model
 
 import (
+	"math"
 	"reflect"
 	"testing"
 )
@@ -8,6 +9,51 @@ import (
 func TestEmptyPost(t *testing.T) {
 	post := NewPost()
 	if !reflect.DeepEqual(post, &Post{-1, "", post.Published, -1, -1, nil}) {
-		t.Error("topic not empty")
+		t.Error("post not empty")
+	}
+}
+
+func TestValidatePost(t *testing.T) {
+	db, err := GetMockupDB()
+	defer db.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	post := NewPost()
+
+	ok, errs := ValidatePost(db, post)
+	if ok || len(errs) != 3 {
+		t.Error("blank post should not validate")
+	}
+
+	post.UserId = math.MaxInt64
+	ok, errs = ValidatePost(db, post)
+	if ok || len(errs) != 3 {
+		t.Error("this is not a real user")
+	}
+
+	post.UserId = 1
+	ok, errs = ValidatePost(db, post)
+	if ok || len(errs) != 2 {
+		t.Error("valid user, still missing topic and text")
+	}
+
+	post.Text = "Hello!"
+	ok, errs = ValidatePost(db, post)
+	if ok || len(errs) != 1 {
+		t.Error("still missing topic!")
+	}
+
+	post.TopicId = math.MaxInt64
+	ok, errs = ValidatePost(db, post)
+	if ok || len(errs) != 1 {
+		t.Error("should not validate invalid forum")
+	}
+
+	post.TopicId = 2
+	ok, errs = ValidatePost(db, post)
+	if !ok || len(errs) != 0 {
+		t.Error("post should now validate")
 	}
 }
